2e05681b65e5ab95eedd35df513941f070e7e61e
samlarson94 <samlarson94@gmail.com>
2022-04-21 09:41:11 -0500

    Updating server.js file with dependencies and paths

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+app.listen(PORT, () => console.log(`Listening on PORT: PORT`))
